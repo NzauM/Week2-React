9780463e25dc62f8430790ba443ec7c066235a52
mercy.waeni <mercy.waeni@calltronix.com>
2022-07-19 16:52:49 +0300

    App before react routing

--- src/App.js
+++ src/App.js
@@ -11,8 +11,6 @@ import {
   Router,
   useRouteMatch,
 } from "react-router-dom";
-import TodoDetails from "./TodoDetails";
-import Home from "./Home";
 
 function App() {
   const existingTodos = [
@@ -52,32 +50,8 @@ function App() {
 
   return (
     <div className="App">
-    
-      <Switch>
-        {/* <Route path={`/todo/:id`}>
-          <TodoDetails />
-        </Route> */}
-
-        <Route path={`/todoform`}>
-          <TodoForm onFormsubmitted={handleFormSubmitted} />
-        </Route>
-        {/* <Route path={`${match.url}/:todoId`}> */}
-        <Route path={`/todos/:todoId`}>
-          <TodoDetails myTodo={myTodos}></TodoDetails>
-          {/* <TodoForm onFormsubmitted={handleFormSubmitted} /> */}
-        </Route>
-        <Route path="/todos">{showTodos}</Route>
-        <Route path="/"><Home/></Route>
-        
-      </Switch>
-      {/* <BrowserRouter>
-    <Routes>
-
-    <Route path='/todoform' element={<TodoForm onFormsubmitted = {handleFormSubmitted}/>} />
-    <Route path='/' element={showTodos}/>
-
-    </Routes>      
-    </BrowserRouter> */}
+    {showTodos}
+    <TodoForm onFormsubmitted={handleFormSubmitted}></TodoForm>
     </div>
   );
 }
--- src/TodoList.js
+++ src/TodoList.js
@@ -1,32 +1,10 @@
-import { Link, Route, useRouteMatch, useParams } from "react-router-dom"
-import TodoDetails from "./TodoDetails";
-
-
-function TodoList({elem, myindex}){
-    // const params = useParams();
-    // console.log(params)
-    const match = useRouteMatch();
-    console.log(match)
-    // console.log(`${match.url}:${elem.myindex}`)
-    
-    console.log(myindex)
+function TodoList({elem}){
     return(
         <>
             <h1>{elem.task}</h1>
             <p>due in {elem.timeline}</p>
             <button>assigned to; {elem.assignedTo}</button>
-            {/* <Route path={`${match.url}/:${id}`}>
-               <TodoDetails task={task}/>
-               See Details
-            </Route> */}
-            <button>
-            {/* <Link to={`/${myindex}`}>see Details</Link> */}
-            <Link to={`${match.url}/${myindex}`}>see Details</Link>
-            </button>
-          
-             <button>
-            <Link to="/todoform">Go home </Link>
-            </button>
+            
             
         </>
     )
